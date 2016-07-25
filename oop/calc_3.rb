5de10f0aaf000075db9ccd159d234b93e31fa66a
Caroline Joseph <carbroj@gmail.com>
2016-07-25 10:03:42 -0500

    finished calc_3

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -4,8 +4,7 @@
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
-class SimpleCalculator
-
+module SimpleCalculator
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -21,54 +20,16 @@ class SimpleCalculator
   def divide(first_number, second_number)
     first_number / second_number
   end
-
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
+include SimpleCalculator
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
+class WhizBangCalculator < FancyCalculator
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
@@ -83,4 +44,51 @@ class WhizBangCalculator
 end
 
 # Copy your driver code from the previous exercise and more below:
+
+puts "Testing your code for SimpleCalculator..."
+
+calculator = WhizBangCalculator.new
+
+if calculator.add(3, 4) == 7
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.subtract(9, 4) == 5
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.multiply(3, 4) == 12
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.divide(12, 4) == 3
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.square_root(36) == 6
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.hypotenuse(3, 4) == 5
+  puts "Pass!"
+else
+  puts "F!"
+end
+
+if calculator.exponent(2, 3) == 8
+  puts "Pass!"
+else
+  puts "F!"
+end
+
 
